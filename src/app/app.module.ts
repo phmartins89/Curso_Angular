aa8ae96113d0bb6cfe053ea4d258658950d3285a
phmartins89 <ph.martins89@gmail.com>
2020-10-20 11:46:55 -0300

    Criação do Ambiente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {MeuPrimeiroComponent} from './meu-primeiro/meu-primeiro.compenent';
+import { FormsModule } from '@angular/forms';
+import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeuPrimeiroComponent,
+    MeuPrimeiro2Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,    
   ],
   providers: [],
   bootstrap: [AppComponent]
