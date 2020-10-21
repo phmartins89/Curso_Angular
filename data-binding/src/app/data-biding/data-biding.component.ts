e4800a1b29f416cf8dd268b3eba66dafa1f8e36c
phmartins89 <ph.martins89@gmail.com>
2020-10-21 08:32:10 -0300

    Data Binding

---
+++ data-binding/src/app/data-biding/data-biding.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-biding',
+  templateUrl: './data-biding.component.html',
+  styleUrls: ['./data-biding.component.css']
+})
+export class DataBidingComponent implements OnInit {
+
+  url: string ='http://loiane.com'; 
+  cursoAngular: boolean = true;
+  urlImagem = 'https://picsum.photos/seed/picsum/200/300';
+
+  getValor(){
+    return 1;
+  }
+
+  getCurtirCurso(){
+    return true;
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
